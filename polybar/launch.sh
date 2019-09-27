7e672477e2da9d0087bd0567c451fbc2507ff1b8
Dhruvin Shah <dhruvin3@gmail.com>
2019-09-27 09:51:14 -0400

    polybar: make it appear on all monitor

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -7,7 +7,12 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar
-polybar mybar &
-polybar bottombar &
+if type "xrandr"; then
+    for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+        MONITOR=$m polybar --reload mybar &
+    done
+else
+    polybar mybar &
+fi
 
 ehco "Bars launched..."
