44682c09dafdf594c8979d8ade19afaa4e131e22
Dhruvin Shah <33428164+dhruvinsh@users.noreply.github.com>
2020-06-18 21:51:26 -0400

    doom config.el: font changed to default

--- emacs/doom.d/config.el
+++ emacs/doom.d/config.el
@@ -7,7 +7,7 @@
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
 (setq user-full-name "Dhruvin Shah"
-      user-mail-address "dshah@evertz.com")
+      user-mail-address "dhruvin3@gmail.com")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
 ;; are the three important ones:
@@ -19,7 +19,8 @@
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
-(setq doom-font (font-spec :family "CaskaydiaCove Nerd Font Mono" :size 16))
+;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
+;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
