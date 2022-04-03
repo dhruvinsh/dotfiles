a12f8b0791064fcec2368fa1958202a60ae64baf
Dhruvin Shah <dhruvin3@gmail.com>
2022-04-02 21:01:50 -0400

    npm installation extracted and LSP server installation added

---
+++ run_once_after_93-install-node.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+echo "setup node"
+NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+nvm install --lts
+# update npm to latest version
+npm install -g npm
+
+echo "Some LSP installation for development"
+server=(
+    pyright
+    eslint
+    tsserver
+)
+npm install -g pyright
