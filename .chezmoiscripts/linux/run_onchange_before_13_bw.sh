f41e124748f6964ccb3409c4f6c18c4c54132d07
Dhruvin Shah <dhruvin3@gmail.com>
2022-04-12 09:57:18 -0400

    bitwarden cli added

---
+++ .chezmoiscripts/linux/run_onchange_before_13_bw.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# bitwarden cli setup
+echo "Setting up bitwarden cli"
+URL="https://vault.bitwarden.com/download/?app=cli&platform=linux"
+LOCALBIN=$HOME/.local/bin
+wget $URL -O /tmp/bw.zip
+
+# just incase if we don't have .local/bin directory
+mkdir -p $LOCALBIN
+unzip -o /tmp/bw.zip -d $LOCALBIN
+chmod +x $LOCALBIN/bw
