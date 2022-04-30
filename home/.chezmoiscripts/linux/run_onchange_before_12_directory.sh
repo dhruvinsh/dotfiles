972b8029e3c8db79ab0f7b76798f5695fc919fd8
Dhruvin Shah <dhruvin3@gmail.com>
2022-04-29 22:49:52 -0400

    shellcheck error fixed for github actions

--- home/.chezmoiscripts/linux/run_onchange_before_12_directory.sh
+++ home/.chezmoiscripts/linux/run_onchange_before_12_directory.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 # create tpm directory
 # chezmoi does not create parent directories
 # https://github.com/twpayne/chezmoi/issues/1205
-mkdir -p $HOME/.tmux/plugins/tpm
+mkdir -p "$HOME"/.tmux/plugins/tpm
