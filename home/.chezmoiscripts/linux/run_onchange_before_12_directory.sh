8af83be2b5711d1c44de5aa162ccee371a365a8a
Dhruvin Shah <dhruvin3@gmail.com>
2022-05-13 13:34:06 -0400

    directory creation is not required

--- home/.chezmoiscripts/linux/run_onchange_before_12_directory.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-# create tpm directory
-# chezmoi does not create parent directories
-# https://github.com/twpayne/chezmoi/issues/1205
-mkdir -p "$HOME"/.tmux/plugins/tpm
