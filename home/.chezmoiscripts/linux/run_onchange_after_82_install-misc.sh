7d579e0c0aa080f9d7581e939395217294cbfa7f
Dhruvin Shah <dhruvin3@gmail.com>
2022-11-03 22:50:15 -0400

    tool: replacing autojump with zoxide

--- home/.chezmoiscripts/linux/run_onchange_after_82_install-misc.sh
+++ home/.chezmoiscripts/linux/run_onchange_after_82_install-misc.sh
@@ -1,17 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-echo "update font cache"
+echo "Update font cache"
 fc-cache -f -v
 
-echo "setup autojump"
-AUTOJUMP=https://github.com/wting/autojump.git
-AUTOJUMP_DIR=/tmp/autojump
-if [ ! -d $AUTOJUMP_DIR ]; then
-  git clone $AUTOJUMP $AUTOJUMP_DIR
-fi
-cd $AUTOJUMP_DIR
-PYENV_ROOT="$HOME/.pyenv"
-PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init --path)"
-./install.py
+echo "Setup zoxide"
+ZOXIDE=https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh
+curl -sS $ZOXIDE | bash
