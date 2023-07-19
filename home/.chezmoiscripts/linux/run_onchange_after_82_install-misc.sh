75baaf3c3be3a6018389ee7886a2aecc4b2e802d
Dhruvin Shah <dhruvin3@gmail.com>
2023-07-19 10:05:57 -0400

    scripts: proper vim filetype and commenting jinja

--- home/.chezmoiscripts/linux/run_onchange_after_82_install-misc.sh
+++ home/.chezmoiscripts/linux/run_onchange_after_82_install-misc.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# vim: set expandtab ft=bash ts=4 tw=4:
 set -euo pipefail
 
 echo "Update font cache"
--- home/.chezmoiscripts/linux/run_onchange_after_86_install-rust.sh
+++ home/.chezmoiscripts/linux/run_onchange_after_86_install-rust.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# vim: set expandtab ft=bash ts=4 tw=4:
 set -euo pipefail
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
