d4523b0496a36959d005bf0a6df91831d6e573ac
Dhruvin Shah <dhruvin3@gmail.com>
2023-07-19 10:05:31 -0400

    external: moving zoxide to external directive

--- home/.chezmoiscripts/linux/run_onchange_after_82_install-misc.sh
+++ home/.chezmoiscripts/linux/run_onchange_after_82_install-misc.sh
@@ -3,7 +3,3 @@ set -euo pipefail
 
 echo "Update font cache"
 fc-cache -f -v
-
-echo "Setup zoxide"
-ZOXIDE=https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh
-curl -sS $ZOXIDE | bash
