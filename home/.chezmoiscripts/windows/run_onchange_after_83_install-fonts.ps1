3737beea519ca05bfe9bc1087402c1f4332d20ff
Dhruvin Shah <dhruvin3@gmail.com>
2023-07-19 22:08:58 -0400

    script: windows font installation added

---
+++ home/.chezmoiscripts/windows/run_onchange_after_83_install-fonts.ps1
@@ -0,0 +1,20 @@
+# vim: set ft=powershell
+
+# lets make sure scoop have nerd-fonts added
+scoop bucket add nerd-fonts
+
+# NOTE: do not install 7zip from scoop else we will have issue
+# for file extraction for scoop
+$fonts = @(
+  "nerd-fonts/CascadiaCode-NF"
+  "nerd-fonts/FiraCode-NF"
+  "nerd-fonts/Iosevka-NF"
+  "nerd-fonts/JetBrainsMono-NF"
+  "nerd-fonts/VictorMono-NF"
+);
+
+
+foreach ($font in $fonts) {
+    Write-Output "--> Installing $font.."
+    scoop install $font
+}
