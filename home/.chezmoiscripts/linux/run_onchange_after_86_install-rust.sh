087f8c73cf8397cd1124bbabdc5fee10888827f7
Dhruvin Shah <dshah@evertz.com>
2023-08-09 13:50:16 -0400

    package: diffr added

--- home/.chezmoiscripts/linux/run_onchange_after_86_install-rust.sh
+++ home/.chezmoiscripts/linux/run_onchange_after_86_install-rust.sh
@@ -3,3 +3,10 @@
 set -euo pipefail
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+cargo_packages=(
+    diffr
+)
+
+[ -s "$HOME/.cargo/env" ] && source "$HOME/.cargo/env"
+cargo install "${cargo_packages[@]}"
