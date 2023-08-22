39fc66c574ce897d9fc5741813941a1b4128a49d
Dhruvin Shah <dhruvin3@gmail.com>
2023-08-21 22:07:26 -0400

    config: wezterm added

---
+++ home/private_dot_config/wezterm/keymaps.lua
@@ -0,0 +1,61 @@
+local wezterm = require("wezterm")
+local platform = require("utils.platform")
+
+local action = wezterm.action
+
+-- | name            | MacOS     | Windows
+-- ---------------------------------------
+-- | SUPER, CMD, WIN | Command   | Win
+-- | CTRL            | Ctrl      | Ctrl
+-- | SHIFT           | Shift     | Shift
+-- | ALT, OPT, META  | Option    | Alt
+local mod = "SHIFT|SUPER"
+local alt = "SUPER"
+if platform.is_win or platform.is_linux then
+  mod = "SHIFT|ALT"
+  alt = "ALT"
+end
+
+local M = {}
+
+---@param cfg table
+M.setup = function(cfg)
+  -- disable all the default mappings
+  cfg.disable_default_key_bindings = true
+
+  -- leader key: CTRL + SPACE
+  cfg.leader = { key = "Space", mods = "CTRL", timeout_milliseconds = 1000 }
+
+  cfg.keys = {
+    -- window management: splits, movement, spawn
+    { mods = mod, key = "|", action = action.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+    { mods = mod, key = "-", action = action.SplitVertical({ domain = "CurrentPaneDomain" }) },
+
+    { mods = alt, key = "UpArrow", action = action.ActivatePaneDirection("Up") },
+    { mods = alt, key = "DownArrow", action = action.ActivatePaneDirection("Down") },
+    { mods = alt, key = "LeftArrow", action = action.ActivatePaneDirection("Left") },
+    { mods = alt, key = "RightArrow", action = action.ActivatePaneDirection("Right") },
+
+    { mods = mod, key = "d", action = action.SpawnTab("CurrentPaneDomain") },
+
+    -- text management: copy-paste
+    { mods = "CTRL|SHIFT", key = "c", action = action.CopyTo("Clipboard") },
+    { mods = "CTRL|SHIFT", key = "v", action = action.PasteFrom("Clipboard") },
+  }
+
+  cfg.mouse_bindings = {
+    {
+      mods = "CTRL",
+      event = { Up = { streak = 1, button = "Left" } },
+      action = action.OpenLinkAtMouseCursor,
+    },
+    -- Disable the 'Down' event of CTRL-Click to avoid weird program behaviors
+    {
+      event = { Down = { streak = 1, button = "Left" } },
+      mods = "CTRL",
+      action = action.Nop,
+    },
+  }
+end
+
+return M
---
+++ home/private_dot_config/wezterm/ui.lua
@@ -0,0 +1,63 @@
+local colorscheme = "nordfox"
+local wezterm = require("wezterm")
+
+local regular_font = "JetBrainsMono Nerd Font"
+local italic_font = "VictorMono Nerd Font"
+
+local M = {}
+
+---@param cfg table
+M.setup = function(cfg)
+  -- font setting
+  cfg.font = wezterm.font(regular_font)
+  cfg.font_size = 14.5
+  cfg.font_rules = {
+    {
+      italic = true,
+      intensity = "Normal",
+      font = wezterm.font({ family = italic_font, style = "Italic" }),
+    },
+    {
+      italic = true,
+      intensity = "Half",
+      font = wezterm.font({
+        family = italic_font,
+        weight = "DemiBold",
+        style = "Italic",
+      }),
+    },
+    {
+      italic = true,
+      intensity = "Bold",
+      font = wezterm.font({
+        family = italic_font,
+        weight = "Bold",
+        style = "Italic",
+      }),
+    },
+  }
+
+  -- color scheme
+  cfg.color_scheme = colorscheme
+
+  -- tab bar config
+  -- nordfox : use_fancy_tab_bar: false
+  cfg.use_fancy_tab_bar = false
+  cfg.tab_max_width = 25
+
+  cfg.enable_scroll_bar = false
+
+  cfg.window_padding = {
+    left = "0.5cell",
+    right = "0.5cell",
+    top = 0,
+    bottom = 0,
+  }
+
+  cfg.inactive_pane_hsb = {
+    saturation = 0.9,
+    brightness = 0.8,
+  }
+end
+
+return M
