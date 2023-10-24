b9543452aceb71f607eab901852ac3dfff333065
Dhruvin Shah <dhruvin3@gmail.com>
2023-10-24 16:22:32 -0400

    config(wezterm): adding support for zen-mode

--- home/private_dot_config/wezterm/wezterm.lua
+++ home/private_dot_config/wezterm/wezterm.lua
@@ -3,6 +3,7 @@ local ui = require("ui")
 local program = require("program")
 local keymaps = require("keymaps")
 local tab_title = require("events.tab_title")
+local zen_mode = require("events.zen_mode")
 
 local config = {}
 
@@ -14,5 +15,6 @@ ui.setup(config)
 program.setup(config)
 keymaps.setup(config)
 tab_title.setup(ui.colors)
+zen_mode.setup()
 
 return config
