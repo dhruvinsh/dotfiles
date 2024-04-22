310f1cd8232065ae012ecfb5e36b1386a2468a8b
Dhruvin Shah <dhruvin3@gmail.com>
2024-04-21 21:23:37 -0400

    feat(wezterm): better color scheme and refactoring the code
    * font get its own module
    * adding hybrid color scheme

--- home/private_dot_config/wezterm/ui.lua
+++ home/private_dot_config/wezterm/ui.lua
@@ -1,14 +1,10 @@
 local wezterm = require("wezterm")
 local platform = require("utils.platform")
 
-local regular_font = "Iosevka NF"
-local italic_font = "VictorMono NF"
-local frame_font = "Iosevka NF"
-
 local M = {}
 
 local ok
-M.colorscheme = "rose-pine"
+M.colorscheme = "hybrid-local"
 ok, M.colors =
   pcall(wezterm.color.load_scheme, wezterm.home_dir .. "/.config/wezterm/colors/" .. M.colorscheme .. ".toml")
 if not ok then
@@ -18,45 +14,9 @@ end
 
 ---@param cfg table
 M.setup = function(cfg)
-  -- font setting
-  cfg.window_frame = { font = wezterm.font(frame_font) } -- set frame fonts, affect the command palette as well
   cfg.front_end = "WebGpu"
   if platform.is_win then
     cfg.webgpu_power_preference = "HighPerformance"
-  end
-  cfg.freetype_load_flags = "NO_HINTING"
-  cfg.font = wezterm.font({ family = regular_font, weight = "Regular" })
-  cfg.font_rules = {
-    {
-      italic = true,
-      intensity = "Normal",
-      font = wezterm.font({ family = italic_font, style = "Italic" }),
-    },
-    {
-      italic = true,
-      intensity = "Half",
-      font = wezterm.font({
-        family = italic_font,
-        weight = "DemiBold",
-        style = "Italic",
-      }),
-    },
-    {
-      italic = true,
-      intensity = "Bold",
-      font = wezterm.font({
-        family = italic_font,
-        weight = "Bold",
-        style = "Italic",
-      }),
-    },
-  }
-  if platform.is_mac or platform.is_linux then
-    cfg.font_size = 15.0
-    cfg.command_palette_font_size = 14.0
-  else
-    cfg.font_size = 13.0
-    cfg.command_palette_font_size = 12.5
   end
 
   -- color scheme
