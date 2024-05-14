37b80c4beb652ada71ab83a06f2f79708d372b36
Dhruvin Shah <dhruvin3@gmail.com>
2024-05-14 14:33:14 -0400

    feat(wezterm): better wsl config

--- home/private_dot_config/wezterm/program.lua
+++ home/private_dot_config/wezterm/program.lua
@@ -1,17 +1,33 @@
+local wezterm = require("wezterm")
 local platform = require("utils.platform")
+local wsl = require("wsl")
 local M = {}
 
 ---@param cfg table
 M.setup = function(cfg)
   if platform.is_win then
-    cfg.default_prog = { "wsl", "--cd", "~" }
-    cfg.launch_menu = {
-      { label = "WSL", args = { "wsl", "--cd", "~" } },
-      { label = "PowerShell Core", args = { "pwsh" } },
-      { label = "PowerShell Desktop", args = { "powershell.exe", "-NoLogo" } },
-      { label = "Command Prompt", args = { "cmd" } },
-      { label = "Nushell", args = { "nu" } },
+    local ip = wsl.get_ip()
+    if not ip then
+      wezterm.log_error("Could not able to find WSL IP address")
+      cfg.default_domain = "local"
+    end
+
+    cfg.ssh_domains = {
+      {
+        name = "wsl:ssh",
+        remote_address = ip,
+        username = "ds",
+        multiplexing = "None",
+        default_prog = { "zsh" },
+        assume_shell = "Posix",
+      },
     }
+    cfg.launch_menu = {
+      { label = "PowerShell Core", args = { "pwsh" }, domain = { DomainName = "local" } },
+      { label = "Command Prompt", args = { "cmd" }, domain = { DomainName = "local" } },
+      { label = "Nushell", args = { "nu" }, domain = { DomainName = "local" } },
+    }
+    cfg.default_domain = "wsl:ssh"
   elseif platform.is_mac then
     cfg.default_prog = { "zsh", "-l" }
     cfg.launch_menu = {
