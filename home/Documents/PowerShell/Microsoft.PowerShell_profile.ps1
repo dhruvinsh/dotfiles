6de8def03e963ba543b35334cd84be07440b07bf
Dhruvin Shah <dhruvin3@gmail.com>
2024-09-11 18:54:51 -0400

    feat: windows add volta completion for pwsh

--- home/Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ home/Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -4,3 +4,8 @@ if (Get-Command zoxide -errorAction SilentlyContinue)
   Set-Alias z zoxide
   Invoke-Expression (& { (zoxide init powershell | Out-String) })
 }
+
+if (Get-Command volta -errorAction SilentlyContinue)
+{
+  (& volta completions powershell) | Out-String | Invoke-Expression
+}
