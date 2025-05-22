182d03e5dc669905fe0cf4576b12b77eff77caab
Dhruvin Shah <dhruvin3@gmail.com>
2025-05-22 19:34:38 -0400

    feat(wezterm): reorder module imports and setup calls
    Reorganized the import statements and setup calls in the WezTerm configuration file for better readability and logical grouping. No functional changes were made.

--- home/private_dot_config/wezterm/wezterm.lua
+++ home/private_dot_config/wezterm/wezterm.lua
@@ -1,26 +1,28 @@
 local wezterm = require("wezterm")
-local ui = require("ui")
-local fonts = require("fonts")
-local program = require("program")
-local keymaps = require("keymaps")
-local tab_title = require("events.tab_title")
-local zen_mode = require("events.zen_mode")
-local domain = require("events.domain")
-local full_screen = require("events.full_screen")
 local colors = require("colors")
+local domain = require("events.domain")
+local fonts = require("fonts")
+local full_screen = require("events.full_screen")
+local keymaps = require("keymaps")
+local program = require("program")
+local tab_title = require("events.tab_title")
+local ui = require("ui")
+local zen_mode = require("events.zen_mode")
 
 local config = wezterm.config_builder()
 
 -- NOTE: first load colors
 colors.setup(config)
 
-ui.setup(config)
 fonts.setup(config)
-program.setup(config)
 keymaps.setup(config)
-tab_title.setup(config.colors)
-zen_mode.setup()
+program.setup(config)
+ui.setup(config)
+
+-- Events
 domain.setup()
 full_screen.setup()
+tab_title.setup(config.colors)
+zen_mode.setup()
 
 return config
