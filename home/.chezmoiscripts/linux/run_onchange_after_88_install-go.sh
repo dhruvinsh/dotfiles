720414ecf3d53f73fdb5a7fb467a648a5b38f489
Dhruvin Shah <dhruvin3@gmail.com>
2025-06-28 20:40:20 -0400

    fix: skip certain scripts and configs on Arch Linux
    Wrap scripts and configuration in conditional checks to prevent execution on Arch Linux, ensuring compatibility and avoiding unnecessary steps on that platform.

--- home/.chezmoiscripts/linux/run_onchange_after_88_install-go.sh
+++ home/.chezmoiscripts/linux/run_onchange_after_88_install-go.sh
@@ -2,6 +2,8 @@
 # vim: set expandtab ft=bash ts=4 tw=4:
 set -euo pipefail
 
+# {{ if not .archlinux -}}
 sudo add-apt-repository -y ppa:longsleep/golang-backports
 sudo apt update
 sudo apt install -y golang-go
+# {{ end }}
